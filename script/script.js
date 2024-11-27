33bc84fefadcb0204c8466a0ee8923a8bfef366d
Kadir Zeybekoğlu <119623139+kadrzeybek@users.noreply.github.com>
2024-11-27 23:37:59 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector(".navbar");
+
+const menuBtn = document.querySelector(".menu-btn");
+
+
+menuBtn.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    document.addEventListener("click", function(e){
+        if(!e.composedPath().includes(menuBtn) && !e.composedPath().includes(navbar)){
+            navbar.classList.remove("active");
+        }
+    })
+});
